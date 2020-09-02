d46696ab14f4eca032e1c9e177f997c9f8a3fcf4
Kokuyo3 <chertang3@gmail.com>
2020-09-01 22:33:08 -0700

    Add api route for getting a track features

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const querystring = require('querystring');
 const SpotifyApiController = require('../controllers/SpotifyApiController');
 
 const router = express.Router();
 
-router.get('/getTrack/:id', (req, res) => {
+router.get('/track/:id', (req, res) => {
   SpotifyApiController.getTrack(req.params.id).then((trackData) => { res.json(trackData); })
     .catch((error) => { console.log(error); });
 });
@@ -12,6 +11,12 @@ router.get('/getTrack/:id', (req, res) => {
 router.get('/search', (req, res) => {
   SpotifyApiController.search(req.query.q, req.query.type, req.query.market, req.query.limit)
     .then((results) => { res.json(results); })
+    .catch((error) => { console.log(error); });
+});
+
+router.get('/track-features/:id', (req, res) => {
+  SpotifyApiController.getTrackFeatures(req.params.id)
+    .then((trackFeaturesData) => { res.json(trackFeaturesData); })
     .catch((error) => { console.log(error); });
 });
 
