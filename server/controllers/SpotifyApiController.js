65c37a5ff2e51caf1e05659bd0850b7296c68770
Kokuyo3 <chertang3@gmail.com>
2020-09-11 21:12:12 -0700

    Update /search endpoint to return parsed data with audio features

--- server/controllers/SpotifyApiController.js
+++ server/controllers/SpotifyApiController.js
@@ -14,12 +14,101 @@ class SpotifyApiController {
         baseURL: 'https://api.spotify.com/v1',
         headers: this.authHeader,
       });
-
-      console.log(this.authHeader); // TODO: remove
     }).catch((error) => {
       console.error(error);
     });
   }
+
+  authorizeApp = async () => {
+    const authHeader = `Basic ${Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`)
+      .toString('base64')}`;
+
+    const options = {
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      headers: { Authorization: authHeader },
+      data: querystring.encode({ grant_type: 'client_credentials' }),
+    };
+
+    try {
+      const response = await axios(options);
+
+      return response.data.access_token;
+    } catch (error) {
+      this.handleHttpError(error);
+
+      return null;
+    }
+  };
+
+  search = async (q, type = 'track', market = 'US', limit = '20') => {
+    try {
+      const { data: { tracks } } = await this.spotifyApi.get('/search', {
+        params: {
+          q,
+          type,
+          market,
+          limit,
+        },
+      });
+
+      const itemIds = [];
+
+      const parsedData = {
+        href: tracks.href,
+        next: tracks.next,
+        previous: tracks.previous,
+        offset: tracks.offset,
+        total: tracks.total,
+        items: [],
+      };
+
+      tracks.items.forEach((item) => {
+        if (item.type === 'track') {
+          itemIds.push(item.id);
+
+          parsedData.items.push({
+            albumId: item.album.id,
+            albumArtUrl: item.album.images[1].url,
+            albumName: item.album.name,
+            artists: item.artists,
+            id: item.id,
+            name: item.name,
+          });
+        } else {
+          throw new Error(`Unhandled search result item type: ${item.type}\n\t${item}`);
+        }
+      });
+
+      const { data: { audio_features: audioFeatures } } = await this.spotifyApi.get('/audio-features',
+        { params: { ids: itemIds.join(',') } });
+
+      const featuresMap = new Map();
+
+      audioFeatures.forEach((trackFeature) => {
+        featuresMap.set(trackFeature.id, {
+          danceability: trackFeature.danceability,
+          energy: trackFeature.energy,
+          key: trackFeature.key,
+          mode: trackFeature.mode,
+          instrumentalness: trackFeature.instrumentalness,
+          valence: trackFeature.valence,
+          tempo: trackFeature.tempo,
+          duration: trackFeature.duration_ms,
+        });
+
+        parsedData.items = parsedData.items.map((item) => (
+          { ...item, features: featuresMap.get(item.id) }
+        ));
+      });
+
+      return parsedData;
+    } catch (error) {
+      this.handleHttpError(error);
+
+      return null;
+    }
+  };
 
   getTrack = async (trackId) => {
     try {
@@ -35,56 +124,18 @@ class SpotifyApiController {
 
   getTrackFeatures = async (trackId) => {
     try {
-      const response = await this.spotifyApi.get(`/audio-features/${trackId}`);
+      const { data } = await this.spotifyApi.get(`/audio-features/${trackId}`);
 
       return {
-        key: response.data.key,
-        mode: response.data.mode,
-        danceability: response.data.danceability,
-        energy: response.data.energy,
-        tempo: response.data.tempo,
-        id: response.data.id,
+        danceability: data.danceability,
+        energy: data.energy,
+        key: data.key,
+        mode: data.mode,
+        instrumentalness: data.instrumentalness,
+        valence: data.valence,
+        tempo: data.tempo,
+        duration: data.duration_ms,
       };
-    } catch (error) {
-      this.handleHttpError(error);
-
-      return null;
-    }
-  }
-
-  search = async (q, type = 'track', market = 'US', limit = '15') => {
-    try {
-      const response = await this.spotifyApi.get('/search', {
-        params: {
-          q,
-          type,
-          market,
-          limit,
-        },
-      });
-
-      return await response.data;
-    } catch (error) {
-      this.handleHttpError(error);
-
-      return null;
-    }
-  }
-
-  authorizeApp = async () => {
-    const authHeader = `Basic ${Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`;
-
-    const options = {
-      method: 'post',
-      url: 'https://accounts.spotify.com/api/token',
-      headers: { Authorization: authHeader },
-      data: querystring.encode({ grant_type: 'client_credentials' }),
-    };
-
-    try {
-      const response = await axios(options);
-
-      return response.data.access_token;
     } catch (error) {
       this.handleHttpError(error);
 
