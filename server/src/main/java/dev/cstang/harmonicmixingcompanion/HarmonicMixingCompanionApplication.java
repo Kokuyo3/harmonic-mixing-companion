5145e3536e24126f04bc69705259fc9b3367724b
Cheryl T <Kokuyo3@users.noreply.github.com>
2021-03-15 15:37:40 -0700

    HMC-2 - Begin converting backend into Spring Boot + Java  (#3)
    Replace the old JS server backend with a Spring Boot boilerplate.
    
    * HMC-2 - Move old JS backend server files
    * HMC-2 - Add Spring backend server boilerplate
    * HMC-2 - Merge top-level package.json into client package.json
    * HMC-2 - Ignore application-dev.properties
    * HMC-2 - Override Spring's default Logback with log4j2
    * HMC-2 - Remove old JS backend server files

---
+++ server/src/main/java/dev/cstang/harmonicmixingcompanion/HarmonicMixingCompanionApplication.java
@@ -0,0 +1,13 @@
+package dev.cstang.harmonicmixingcompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonicMixingCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonicMixingCompanionApplication.class, args);
+	}
+
+}
